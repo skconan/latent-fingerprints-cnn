d811530a735a662d1d1ab169adb645eb283d1b47
skconan <supakit.kr@gmail.com>
2019-12-28 22:55:36 +0700

    add constant vars

--- source/constansts.py
+++ source/constansts.py
@@ -15,5 +15,13 @@ PATH_WS = os.path.dirname(os.path.abspath(__file__)).replace("source", "")[:-1]
 PATH_DATASET = PATH_WS + r"\dataset"
 PATH_INPUT = PATH_DATASET + r"\input"
 PATH_TARGET = PATH_DATASET + r"\target"
+
 PATH_INPUT_SEG = PATH_INPUT + r"\segmented"
 PATH_TARGET_SEG = PATH_TARGET + r"\segmented"
+
+PATH_TARGET_SEG_STFT = PATH_TARGET + r"\segmented_stft"
+
+PATH_INPUT_TRAIN_STFT = PATH_INPUT + r"\train_stft"
+PATH_INPUT_VALID_STFT = PATH_INPUT + r"\valid_stft"
+PATH_TARGET_TRAIN_STFT = PATH_TARGET + r"\train_stft"
+PATH_TARGET_VALID_STFT = PATH_TARGET + r"\valid_stft"
