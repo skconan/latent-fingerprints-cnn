323f39dc7af4f3632843a63b77b2df3fadbe5f5b
skconan <supakit.kr@gmail.com>
2019-12-28 22:55:17 +0700

    autoencoder layer for train spectral

--- source/model.py
+++ source/model.py
@@ -1,4 +1,4 @@
-  
+
 """
     File name: model.py
     Author: skconan
@@ -18,8 +18,18 @@ from keras.callbacks import EarlyStopping
 from keras.layers import Input, Activation, BatchNormalization, Conv2D, MaxPooling2D, Dropout, UpSampling2D
 from keras.callbacks import ModelCheckpoint
 from mycallback import MyCallback
-
+import tensorflow as tf
+import tensorflow_model_optimization as tfmot
+from keras import backend as K
 from fft import *
+
+def custom_loss(y_true, y_pred):
+    max_pixel = 1.0
+    return (10.0 * K.log((max_pixel ** 2) / (K.mean(K.square(y_pred - y_true), axis=-1)))) / 2.303
+    # np.count_nonzero(y_true)
+
+    # return (y_true * y_pred) / (K.abs(y_true) *K.abs(y_pred))
+    # return K.abs(y_true - y_pred)
 
 class Autoencoder:
     def __init__(self, model_dir, pred_dir, img_rows=64, img_cols=64, channels=1):
@@ -30,14 +40,20 @@ class Autoencoder:
         self.model_dir = model_dir
         self.pred_dir = pred_dir
 
-        #adm = Adam(lr=1e-3, beta_1=0.9, beta_2=0.999, epsilon=None, decay=1e-6, amsgrad=True)
-        sgd = SGD(lr=0.01, decay=1e-4, momentum=0.9, nesterov=True)
+        self.encoding_dim = 64
+
+        adm = Adam(lr=1e-4, beta_1=0.9, beta_2=0.999, decay=1e-6, amsgrad=True)
+        # sgd = SGD(lr=0.001, decay=1e-4, momentum=0.9, nesterov=True)
         #sgd = SGD(lr=0.01, decay=1e-7, momentum=0.1, nesterov=True)
         #sgd = SGD(lr=0.01, decay=1e-3, momentum=0.8, nesterov=True)
-        rms = RMSprop(lr=0.0001, rho=0.9, epsilon=None, decay=1e-6)        
+        rms = RMSprop(lr=0.001, rho=0.9)
         self.autoencoder_model = self.build_model()
         self.autoencoder_model.compile(
-            loss='mse', optimizer=rms, metrics=['acc'])
+            # loss='mse', optimizer=rms, metrics=['acc'])
+            # loss='sparse_categorical_crossentropy', optimizer=rms, metrics=['acc'])
+            loss='mae', optimizer=rms, metrics=['acc'])
+            # loss=custom_loss, optimizer=adm, metrics=['acc'])
+        # loss='mse', optimizer=sgd, metrics=['acc'])
 
         self.autoencoder_model.summary()
 
@@ -52,28 +68,29 @@ class Autoencoder:
 
         return x
 
-    def upsampling(self, input_layer, number_of_filter):
+    def upsampling(self, input_layer, number_of_filter,dropout=False):
         x = Conv2D(number_of_filter, (3, 3), activation='relu',
                    padding='same')(input_layer)
+        if dropout:
+            x = Dropout(0.5)(x)
         x = UpSampling2D((2, 2))(x)
-
         return x
 
     def build_model(self):
         input_layer = Input(
             shape=(self.img_rows, self.img_cols, self.channels))
 
-        x = self.downsampling(input_layer, 128)
+        x = self.downsampling(input_layer, self.encoding_dim, dropout=False)
         # x = self.downsampling(x, 128)
-        x = self.downsampling(x, 256)
-        x = self.downsampling(x, 512, dropout=True)
+        x = self.downsampling(x, self.encoding_dim * 2, dropout=False)
+        # x = self.downsampling(x, self.encoding_dim * 4, dropout=False)
 
-        x = self.downsampling(x, 1024, dropout=True, maxpooling=False)
+        x = self.downsampling(x, self.encoding_dim * 4, dropout=False, maxpooling=False)
 
-        x = self.upsampling(x, 512)
-        x = self.upsampling(x, 256)
+        # x = self.upsampling(x, self.encoding_dim * 4, dropout=False)
+        x = self.upsampling(x, self.encoding_dim * 2, dropout=False)
         # x = self.upsampling(x, 128)
-        x = self.upsampling(x, 128)
+        x = self.upsampling(x, self.encoding_dim, dropout=False)
 
         output_layer = Conv2D(
             self.channels, (3, 3), activation='sigmoid', padding='same')(x)
@@ -88,15 +105,32 @@ class Autoencoder:
                                        mode='auto')
 
         filepath = self.model_dir + "/model-{epoch:03d}-{val_loss:.4f}.hdf5"
-
+        print(self.model_dir)
         checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1,
                                      save_best_only=True, save_weights_only=False, mode='min')
-
-        my_callback = MyCallback(x_val, self.model_dir, self.pred_dir)
+        # print(checkpoint)
+        my_callback = MyCallback(x_val, y_val, self.model_dir, self.pred_dir)
         callbacks_list = [
             checkpoint,
             my_callback
         ]
+        # print("x_train",x_train)
+        # print("y_train",y_train)
+        # print("x_val",x_val)
+        # print("y_val",y_val)
+        # print("batch_size",batch_size)
+        # print("epochs",epochs)
+        # end_step = np.ceil(1.0 * len(x_train) / batch_size).astype(np.int32) * epochs
+        # print("End step:",end_step)
+        # pruning_schedule = tfmot.sparsity.keras.PolynomialDecay(
+        #     initial_sparsity=0.0, final_sparsity=0.5,
+        #     begin_step=0, end_step=end_step, frequency=100
+        # )
+
+        # self.autoencoder_model = tfmot.sparsity.keras.prune_low_magnitude(self.autoencoder_model, pruning_schedule=pruning_schedule)
+
+        # history = self.autoencoder_model.fit(x_train, y_train,
+        
         history = self.autoencoder_model.fit(x_train, y_train,
                                              batch_size=batch_size,
                                              epochs=epochs,
@@ -114,11 +148,9 @@ class Autoencoder:
         plt.close()
 
     def eval_model(self, x_test):
-        x_test = freq2spatial(x_test)
         preds = self.autoencoder_model.predict(x_test)
+        # preds = freq2spatial(preds)
         return preds
 
     def save(self, path):
         self.autoencoder_model.save(path)
-
-
